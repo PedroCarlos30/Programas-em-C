f39206014dd7c419ecdfd87e663f725a658db28b
Pedro Carlos <58538765+PedroCarlos30@users.noreply.github.com>
2024-03-01 08:58:11 -0300

    Add files via upload

---
+++ dolarparareal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+    float dolar, real;
+
+    printf("Qual a atual cotacão do dolar:\n");
+    scanf("%f", &dolar);
+
+    printf("Qual o valor em reais:\n");
+    scanf("%f", &real);
+
+    printf("A conversão é R$%.2f.\n", real/dolar);
+
+	system("pause");
+    return 0;
+}
