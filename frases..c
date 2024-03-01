2fc88bf355b5c83018c87003cf024fd44f3352e3
Pedro Carlos <58538765+PedroCarlos30@users.noreply.github.com>
2024-03-01 08:46:10 -0300

    Add files via upload

---
+++ frases..c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+    printf("É bom programar em C.\n\n Espero ter um \n bom aprendisado.\n\n\t Boa aula.\n\n Programação é \n\t legal.");
+    return 0;
+}
